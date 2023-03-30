7495157205bedf4c558ceb6cd3bd99a8db254be9
sanjiv <sanjivgautamofficial@gmail.com>
2023-03-30 14:00:18 +0545

    feat: initializes nextjs app with tailwind, graphql, react-hook-form,yup; Refactors some code

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,13 @@
-import '@/styles/globals.css'
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+import '@styles/globals.css';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:8000/graphql',
+  cache: new InMemoryCache(),
+});
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <ApolloProvider client={client}>
+    <Component {...pageProps} />
+  </ApolloProvider>
 }
